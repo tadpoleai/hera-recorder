a03ae4f8ff8bff54669e1786701dd9708e969875
zheming.lyu <zheming.lyu@wayz.ai>
2019-09-27 16:36:31 +0800

    wrapper pure js style tron_api

--- client/src/functions/tron_api.js
+++ client/src/functions/tron_api.js
@@ -1,7 +1,6 @@
-import thrift from 'thrift';
-import ControlCommand from './gen-js/tron_types';
-import TronServiceClient from './gen-js/TronService';
 import TronErrno from './tron_errno';
+
+const tronApiUri = 'http://10.211.55.6:9090/tron';
 
 const ConnectionFailedMsg = {
   error: TronErrno.ConnectionFailed,
@@ -10,8 +9,8 @@ const ConnectionFailedMsg = {
 
 async function createDevices(deviceInitializers) {
   try {
-    const transport = new thrift.TXHRTransport('/tron');
-    const protocol = new thrift.TJSONProtocol(transport);
+    const transport = new Thrift.TFramedTransport(tronApiUri);
+    const protocol = new Thrift.TJSONProtocol(transport);
     const client = new TronServiceClient(protocol);
     return client.create_devices(deviceInitializers);
   } catch (error) {
@@ -19,6 +18,61 @@ async function createDevices(deviceInitializers) {
   }
 }
 
+async function getInformations() {
+  try {
+    const transport = new Thrift.TXHRTransport(tronApiUri);
+    const protocol = new Thrift.TJSONProtocol(transport);
+    const client = new TronServiceClient(protocol);
+    const result = {
+      error: 0,
+      reason: 'OK',
+      data: client.get_informations(),
+    };
+    return result;
+  } catch (error) {
+    return ConnectionFailedMsg;
+  }
+}
+
+async function setStorage(folder) {
+  try {
+    const transport = new Thrift.TXHRTransport(tronApiUri);
+    const protocol = new Thrift.TJSONProtocol(transport);
+    const client = new TronServiceClient(protocol);
+    return client.set_storage(folder);
+  } catch (error) {
+    return ConnectionFailedMsg;
+  }
+}
+
+async function adjustDeviceParameters(deviceId, parameters) {
+  try {
+    const transport = new Thrift.TXHRTransport(tronApiUri);
+    const protocol = new Thrift.TJSONProtocol(transport);
+    const client = new TronServiceClient(protocol);
+    return client.adjust_device_parameters(deviceId, parameters);
+  } catch (error) {
+    return ConnectionFailedMsg;
+  }
+}
+
+async function control(command) {
+  try {
+    const transport = new Thrift.TXHRTransport(tronApiUri);
+    const protocol = new Thrift.TJSONProtocol(transport);
+    const client = new TronServiceClient(protocol);
+    return client.control(command, true, 0);
+  } catch (error) {
+    return ConnectionFailedMsg;
+  }
+}
+
 export default {
+  TronErrno,
+  ControlCommand,
   createDevices,
+  getInformations,
+  setStorage,
+  adjustDeviceParameters,
+  control,
 };
